10a3e30dcf842685a596dc0c95362f3740b0876b
dakooooo <102414874+dakooooo@users.noreply.github.com>
2022-04-18 11:30:16 +0800

    Update wzxy-dailyreport.py

--- wzxy-dailyreport.py
+++ wzxy-dailyreport.py
@@ -143,7 +143,7 @@ class WoZaiXiaoYuanPuncher:
             "myArea": "",
             "areacode": "",
             "userId": "",
-            "city_code": os.environ['WZXY_CITYCODE'],
+            "city_code": os.environ['WZXY_CITY_CODE'],
             "timestampHeader": sign_time,
             "signatureHeader": signature
         }
